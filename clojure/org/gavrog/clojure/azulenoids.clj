511a4f66d0405cd493b8d35865028c77d6535dd6
odf <olaf.delgado@gmail.com>
2012-05-13 15:32:55 +1000

    Starte Clojure port of Azulenoid generator.

---
+++ clojure/org/gavrog/clojure/azulenoids.clj
@@ -0,0 +1,14 @@
+(ns org.gavrog.clojure.identify_nets
+  (:import (org.gavrog.jane.numbers Whole)
+           (org.gavrog.joss.dsyms.basic DSymbol DynamicDSymbol)))
+
+(def template
+  (new DSymbol (str "1.1:60:"
+                    "2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 "
+                    "32 34 36 38 40 42 44 46 48 50 52 54 56 58 60,"
+                    "6 3 5 12 9 11 18 15 17 24 21 23 30 27 29 36 "
+                    "33 35 42 39 41 48 45 47 54 51 53 60 57 59,"
+                    "0 0 12 11 28 27 0 0 18 17 36 35 24 23 58 57 30 "
+                    "29 0 0 0 0 42 41 0 0 48 47 0 0 54 53 0 0 60 59 0 0:"
+                    "3 3 3 3 3 3 3 3 3 3,0 0 5 0 7 0 0 0 0 0")))
+
