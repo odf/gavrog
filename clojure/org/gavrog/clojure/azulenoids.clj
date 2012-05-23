be6c5c490942be52f3a39b2716ade3536a7782ed
odf <olaf.delgado@gmail.com>
2012-05-23 10:41:57 +1000

    Use the dot form to create Java class instances.

--- clojure/org/gavrog/clojure/azulenoids.clj
+++ clojure/org/gavrog/clojure/azulenoids.clj
@@ -37,17 +37,17 @@
     (do (.setVDefaultToOne dsx true) (.curvature2D dsx))))
 
 (def template
-  (new DSymbol (str "1.1:60:"
-                    "2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 "
-                    "32 34 36 38 40 42 44 46 48 50 52 54 56 58 60,"
-                    "6 3 5 12 9 11 18 15 17 24 21 23 30 27 29 36 "
-                    "33 35 42 39 41 48 45 47 54 51 53 60 57 59,"
-                    "0 0 12 11 28 27 0 0 18 17 36 35 24 23 58 57 30 "
-                    "29 0 0 0 0 42 41 0 0 48 47 0 0 54 53 0 0 60 59 0 0:"
-                    "3 3 3 3 3 3 3 3 3 3,0 0 5 0 7 0 0 0 0 0")))
+  (DSymbol. (str "1.1:60:"
+                 "2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 "
+                 "32 34 36 38 40 42 44 46 48 50 52 54 56 58 60,"
+                 "6 3 5 12 9 11 18 15 17 24 21 23 30 27 29 36 "
+                 "33 35 42 39 41 48 45 47 54 51 53 60 57 59,"
+                 "0 0 12 11 28 27 0 0 18 17 36 35 24 23 58 57 30 "
+                 "29 0 0 0 0 42 41 0 0 48 47 0 0 54 53 0 0 60 59 0 0:"
+                 "3 3 3 3 3 3 3 3 3 3,0 0 5 0 7 0 0 0 0 0")))
 
 (def octagon
-  (new DSymbol "1.1:16 1:2 4 6 8 10 12 14 16,16 3 5 7 9 11 13 15:8"))
+  (DSymbol. "1.1:16 1:2 4 6 8 10 12 14 16,16 3 5 7 9 11 13 15:8"))
 
 (def boundary-mappings
   (let [template-boundary (boundary-chambers template (Integer. 1) 0 1 2)
@@ -68,11 +68,11 @@
     tmp))
 
 (def octa-sets (filter #(-> %1 max-curvature .isNegative not)
-                       (lazy-seq (new CombineTiles octagon))))
+                       (lazy-seq (CombineTiles. octagon))))
 
 (def octa-syms
   (for [dset octa-sets
-        dsym (lazy-seq (new DefineBranching2d dset 3 2 Whole/ZERO))
+        dsym (lazy-seq (DefineBranching2d. dset 3 2 Whole/ZERO))
         :when (-> dsym .curvature2D .isZero)]
     dsym))
 
