aca514f563cdedb7690209cb689cc2b5ea6b73de
odf <olaf.delgado@gmail.com>
2012-10-07 21:34:11 +1100

    Cleaned up the Boundary class.

--- src/org/gavrog/joss/dsyms/derived/Boundary.java
+++ src/org/gavrog/joss/dsyms/derived/Boundary.java
@@ -1,5 +1,5 @@
 /*
-   Copyright 2005 Olaf Delgado-Friedrichs
+   Copyright 2012 Olaf Delgado-Friedrichs
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -18,41 +18,38 @@ package org.gavrog.joss.dsyms.derived;
 
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.LinkedList;
 
-import org.gavrog.joss.dsyms.basic.DelaneySymbol;
 import org.gavrog.joss.dsyms.basic.DSPair;
+import org.gavrog.joss.dsyms.basic.DelaneySymbol;
 
 
 /**
  * Represents the boundary complex of a topological realization of a Delaney
  * symbol with facet identifications only performed partially.
- * @author Olaf Delgado
- * @version $Id: Boundary.java,v 1.3 2007/04/18 20:19:08 odf Exp $
  */
-public class Boundary {
-    private DelaneySymbol ds;
-    private HashSet onBoundary;
-    private HashMap neighbor;
-    private HashMap count;
+public class Boundary<T> {
+    private DelaneySymbol<T> ds;
+    private HashSet<Face<T>> onBoundary;
+    private HashMap<Face<T>, Face<T>> neighbor;
+    private HashMap<Face<T>, Integer> count;
     
-    static class Face {
+    static class Face<T> {
         int i;
         int j;
-        Object D;
+        T D;
         
-        public Face(int i, Object D, int j) {
+        public Face(final int i, final T D, final int j) {
             this.i = i;
             this.D = D;
             this.j = j;
         }
         
-        public Face(int i, Object D) {
+        public Face(final int i, final T D) {
             this(i, D, -1);
         }
         
-        public Face(Face k, int j) {
+        public Face(final Face<T> k, final int j) {
             this(k.i, k.D, j);
         }
         
@@ -60,7 +57,7 @@ public class Boundary {
             return i;
         }
         
-        public Object getElement() {
+        public T getElement() {
             return D;
         }
         
@@ -72,9 +69,10 @@ public class Boundary {
             return (i * 37 + D.hashCode()) * 37 + j; 
         }
         
-        public boolean equals(Object other) {
+        public boolean equals(final Object other) {
             if (other instanceof Face) {
-                Face k = (Face) other;
+                @SuppressWarnings("unchecked")
+				final Face<T> k = (Face<T>) other;
                 return this.D.equals(k.D) && this.i == k.i && this.j == k.j;
             } else {
                 return false;
@@ -86,37 +84,31 @@ public class Boundary {
      * Constructs a boundary complex with no identifications.
      * @param ds the underlying Delaney symbol.
      */
-    public Boundary(DelaneySymbol ds) {
+    public Boundary(final DelaneySymbol<T> ds) {
         this.ds = ds;
-        this.onBoundary = new HashSet();
-        this.neighbor = new HashMap();
-        this.count = new HashMap();
+        this.onBoundary = new HashSet<Face<T>>();
+        this.neighbor = new HashMap<Face<T>, Face<T>>();
+        this.count = new HashMap<Face<T>, Integer>();
         
-        Iterator idcs1 = ds.indices();
-        while (idcs1.hasNext()) {
-            int i = ((Integer) idcs1.next()).intValue();
-            Iterator elms = ds.elements();
-            while (elms.hasNext()) {
-                Object D = elms.next();
-                onBoundary.add(new Face(i, D));
-                Iterator idcs2 = ds.indices();
-                while (idcs2.hasNext()) {
-                    int j = ((Integer) idcs2.next()).intValue();
-                    count.put(new Face(i, D, j), new Integer(1));
-                    neighbor.put(new Face(i, D, j), new Face(j, D));
+        for (final int i: ds.indices()) {
+            for (final T D: ds.elements()) {
+                onBoundary.add(new Face<T>(i, D));
+                for (final int j: ds.indices()) {
+                    count.put(new Face<T>(i, D, j), 1);
+                    neighbor.put(new Face<T>(i, D, j), new Face<T>(j, D));
                 }
             }
         }
     }
     
     /**
-     * Tests wether a facet is on the boundary.
+     * Tests whether a facet is on the boundary.
      * @param i facet index.
      * @param D element to which the facet belongs.
      * @return true if face i,D is on the boundary.
      */
-    public boolean isOnBoundary(int i, Object D) {
-        return onBoundary.contains(new Face(i, D));
+    public boolean isOnBoundary(final int i, final T D) {
+        return onBoundary.contains(new Face<T>(i, D));
     }
     
     /**
@@ -126,12 +118,12 @@ public class Boundary {
      * @param j second index to specify the ridge.
      * @return the neighbor facet across the ridge.
      */
-    public Face neighbor(int i, Object D, int j) {
+    public Face<T> neighbor(final int i, final T D, final int j) {
         if (!isOnBoundary(i, D)) {
             String text = "(" + i + "," + D + ") not on boundary.";
             throw new IllegalArgumentException(text);
         }
-        return (Face) neighbor.get(new Face(i, D, j));
+        return neighbor.get(new Face<T>(i, D, j));
     }
     
     /**
@@ -141,19 +133,19 @@ public class Boundary {
      * @param j second index to specify the ridge.
      * @return the number of elements glued together along that ridge.
      */
-    public int glueCountAtRidge(int i, Object D, int j) {
+    public int glueCountAtRidge(final int i, final T D, final int j) {
         if (!isOnBoundary(i, D)) {
             String text = "(" + i + "," + D + ") must be on boundary.";
             throw new UnsupportedOperationException(text);
         }
-        return ((Integer) count.get(new Face(i, D, j))).intValue();
+        return count.get(new Face<T>(i, D, j));
     }
     
     /**
      * Perform a specific identification.
      * @param e the edge at which to identify.
      */
-    public void glue(DSPair e) {
+    public void glue(final DSPair<T> e) {
         glue(e.getIndex(), e.getElement());
     }
     
@@ -162,31 +154,31 @@ public class Boundary {
      * @param i the index of the facet to be identified.
      * @param D the element to be identified with its i-neighbor.
      */
-    public void glue(int i, Object D) {
-        Object E = ds.op(i, D);
-        Face iD = new Face(i, D);
-        Face iE = new Face(i, E);
+    public void glue(final int i, final T D) {
+        final T E = ds.op(i, D);
+        final Face<T> iD = new Face<T>(i, D);
+        final Face<T> iE = new Face<T>(i, E);
         if (!onBoundary.contains(iD)) {
             String text = "(" + i + "," + D + ") not on boundary.";
             throw new IllegalArgumentException(text);
         }
 
-        Iterator idcs = ds.indices();
-        while (idcs.hasNext()) {
-            int j = ((Integer) idcs.next()).intValue();
+        for (final int j: ds.indices()) {
             if (j != i) {
-                Face iDjN = (Face) neighbor.get(new Face(i, D, j));
-                Face iEjN = (Face) neighbor.get(new Face(i, E, j));
-                
+                final Face<T> iDjN = neighbor.get(new Face<T>(i, D, j));
+                final Face<T> iEjN = neighbor.get(new Face<T>(i, E, j));
+               
                 int n = glueCountAtRidge(i, D, j) + glueCountAtRidge(i, E, j);
                 
-                Face iDjNk = new Face(iDjN, i + j - iDjN.getFirstIndex());
+                final Face<T> iDjNk =
+                		new Face<T>(iDjN, i + j - iDjN.getFirstIndex());
                 neighbor.put(iDjNk, iEjN);
-                count.put(iDjNk, new Integer(n));
+                count.put(iDjNk, n);
 
-                Face iEjNk = new Face(iEjN, i + j - iEjN.getFirstIndex());
+                final Face<T> iEjNk =
+                		new Face<T>(iEjN, i + j - iEjN.getFirstIndex());
                 neighbor.put(iEjNk, iDjN);
-                count.put(iEjNk, new Integer(n));
+                count.put(iEjNk, n);
             }
         }
         onBoundary.remove(iD);
@@ -200,16 +192,18 @@ public class Boundary {
 	 * @param D the containing element.
 	 * @param queue the queue to store ridges in.
 	 */
-	public void glueAndEnqueue(final int i, final Object D, LinkedList queue) {
-        final Iterator idcs = ds.indices();
-        while (idcs.hasNext()) {
-            final int j = ((Integer) idcs.next()).intValue();
+	public void glueAndEnqueue(
+			final int i,
+			final T D,
+			final LinkedList<Face<T>> queue)
+	{
+		for (final int j: ds.indices()) {
             if (j != i) {
-                final Face f = neighbor(i, D, j);
-                final Object E = f.getElement();
+                final Face<T> f = neighbor(i, D, j);
+                final T E = f.getElement();
                 final int k = f.getFirstIndex();
                 final int l = i + j - k;
-                queue.addLast(new Face(k, E, l));
+                queue.addLast(new Face<T>(k, E, l));
             }
         }
         glue(i, D);
