61ff26aa958dd7a145edc3462a2d83e062db6f08
odf <olaf.delgado@gmail.com>
2012-10-29 00:14:15 +1100

    Made deftype BacktrackingGenerator actually backtrack.

--- clojure/org/gavrog/clojure/permutations.clj
+++ clojure/org/gavrog/clojure/permutations.clj
@@ -1,11 +1,37 @@
 (ns org.gavrog.clojure.permutations)
 
-(deftype BacktrackingGenerator [root children extract desc]
+(defprotocol ResumableGenerator
+  (result [_])
+  (step [_])
+  (checkpoint [_])
+  (resume [_ checkpoint]))
+
+(deftype BacktrackingGenerator [gen-children extract desc stack]
+  ResumableGenerator
+  (result [_] (extract (:node (first stack))))
+  (step [_]
+        (let [stack
+              (if-let [children (seq (gen-children (:node (first stack))))]
+                (conj stack
+                      {:node (first children)
+                       :branch-nr 0
+                       :siblings-left (rest children)})
+                (when-let [stack (seq (drop-while
+                                        #(not (seq (:siblings-left %))) stack))]
+                  (let [{:keys [siblings-left branch-nr]} (first stack)]
+                    (conj (rest stack)
+                          {:node (first siblings-left)
+                           :branch-nr (inc branch-nr)
+                           :siblings-left (rest siblings-left)}))))]
+          (when (seq stack)
+            (BacktrackingGenerator. gen-children extract desc stack))))
   clojure.lang.Seqable
-  (seq [_] (filter (comp not nil?)
-                   (map extract (tree-seq (constantly true) children root))))
+  (seq [bg] (lazy-seq (cons bg (step bg))))
   Object
   (toString [_] (str desc)))
+
+(defn make-backtracker [gen-children extract desc root]
+  (BacktrackingGenerator. gen-children extract desc (list {:node root})))
 
 (defn permutations [n]
   (let [keys (range 1 (inc n))
@@ -20,4 +46,4 @@
                                    :unseen (disj unseen k)}))))
         extract (fn [node] (when (empty? (:unused node)) (:perm node)))
         desc (list `permutations n)]
-        (BacktrackingGenerator. root children extract desc)))
+        (make-backtracker children extract desc root)))
