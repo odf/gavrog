bdcf54ade294aaf9bcccac89c5da5b6997861511
odf <olaf.delgado@gmail.com>
2012-10-28 19:00:30 +1100

    Some more refactoring on permutations.clj

--- clojure/org/gavrog/clojure/permutations.clj
+++ clojure/org/gavrog/clojure/permutations.clj
@@ -1,21 +1,23 @@
 (ns org.gavrog.clojure.permutations)
 
-(deftype BacktrackingGenerator [root complete? branch? children extract]
+(deftype BacktrackingGenerator [root children extract desc]
   clojure.lang.Seqable
-  (seq [gen] (map extract (filter complete? (tree-seq branch? children root)))))
+  (seq [_] (filter (comp not nil?)
+                   (map extract (tree-seq (constantly true) children root))))
+  Object
+  (toString [_] (str desc)))
 
 (defn permutations [n]
   (let [keys (range 1 (inc n))
         root {:perm {}
               :unused (into #{} keys)
               :unseen (into #{} keys)}
-        complete? (comp empty? :unused)
-        branch? (comp seq :unused)
         children (fn [node] (let [{:keys [perm unused unseen]} node]
                               (when-let [i (first unused)]
                                 (for [k unseen]
                                   {:perm (assoc perm i k)
                                    :unused (disj unused i)
                                    :unseen (disj unseen k)}))))
-        extract :perm]
-        (BacktrackingGenerator. root complete? branch? children extract)))
+        extract (fn [node] (when (empty? (:unused node)) (:perm node)))
+        desc (list `permutations n)]
+        (BacktrackingGenerator. root children extract desc)))
