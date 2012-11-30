2b743f599e585d8d75c49116ef7cec858e3e2be5
odf <olaf.delgado@gmail.com>
2012-11-30 18:22:43 +1100

    branchings2d.clj: started writing the backtracking details

--- clojure/org/gavrog/clojure/branchings2d.clj
+++ clojure/org/gavrog/clojure/branchings2d.clj
@@ -1,8 +1,16 @@
 (ns org.gavrog.clojure.branchings
-  (:use (org.gavrog.clojure [generators :only [make-backtracker results]])))
+  (:use (org.gavrog.clojure
+          [generators :only [make-backtracker results]]
+          [delaney])))
+
+(defn canonical? [ds]
+  (= (zipmap (elements ds) (elements ds))
+     (into {} (.getMapToCanonical (java-dsymbol ds)))))
 
 (defn branchings [d-set min-face-degree min-vert-degree min-curvature]
   (make-backtracker 
-    {:root []
-     :extract (fn [[]])
+    {:root (let [orbs (for [i [0 1], D (orbit-reps d-set [i (inc i)])] [i D])]
+             [d-set (into #{} orbs) {}])
+     :extract (fn [[ds unused spins]]
+                (when (and (empty? unused) (canonical? ds)) ds))
      :children (fn [[]])}))
