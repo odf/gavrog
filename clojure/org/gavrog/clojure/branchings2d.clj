3e959b3fb1f52f5fa651fc65e64278eafa8be657
odf <olaf.delgado@gmail.com>
2012-12-01 15:17:26 +1100

    branchings2d.clj: the generator works

--- clojure/org/gavrog/clojure/branchings2d.clj
+++ clojure/org/gavrog/clojure/branchings2d.clj
@@ -3,14 +3,21 @@
           [generators :only [make-backtracker results]]
           [delaney])))
 
-(defn canonical? [ds]
-  (= (zipmap (elements ds) (elements ds))
-     (into {} (.getMapToCanonical (java-dsymbol ds)))))
-
 (defn branchings [d-set min-face-degree min-vert-degree min-curvature]
-  (make-backtracker 
-    {:root (let [orbs (for [i [0 1], D (orbit-reps d-set [i (inc i)])] [i D])]
-             [d-set (into #{} orbs) {}])
-     :extract (fn [[ds unused spins]]
-                (when (and (empty? unused) (canonical? ds)) ds))
-     :children (fn [[]])}))
+  (let [ds (canonical d-set)
+        good-degree (fn [i D v]
+                      (cond (= 0 i) (<= min-face-degree (* v (r ds 0 1 D)))
+                            (= 1 i) (<= min-vert-degree (* v (r ds 1 2 D)))))]
+    (make-backtracker 
+      {:root (let [orbs (for [i [0 1], D (orbit-reps ds [i (inc i)])] [i D])]
+               [ds (into #{} orbs)])
+       :extract (fn [[ds unused]]
+                  (when (and (empty? unused)
+                             (canonical? ds)
+                             (<= min-curvature (curvature ds)))
+                    ds))
+       :children (fn [[ds unused]]
+                   (when-let [[i D] (first unused)]
+                     (for [v [1 2 3 4 6]
+                           :when (good-degree i D v)]
+                       [(spin ds i (inc i) D v) (disj unused [i D])])))})))
