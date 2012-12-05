5a7b6f7157a66efe18b664b1c96cb6997bb4306d
odf <olaf.delgado@gmail.com>
2012-12-05 17:36:01 +1100

    azulenoids.clj: removed most direct Java calls

--- clojure/org/gavrog/clojure/azulenoids.clj
+++ clojure/org/gavrog/clojure/azulenoids.clj
@@ -1,48 +1,46 @@
 (ns org.gavrog.clojure.azulenoids
-  (:use (org.gavrog.clojure [util :only [iterate-cycle unique]]
-                            [delaney :only [s v chain-end curvature]]
-                            [generators :only [results]]
-                            [branchings2d :only [branchings]]))
-  (:import (org.gavrog.jane.numbers Whole)
-           (org.gavrog.joss.dsyms.basic DSymbol DynamicDSymbol IndexList)
-           (org.gavrog.joss.dsyms.generators CombineTiles))
+  (:use (org.gavrog.clojure
+          [util :only [iterate-cycle unique]]
+          [delaney]
+          [generators :only [results]]
+          [branchings2d :only [branchings]]))
+  (:import (org.gavrog.joss.dsyms.generators CombineTiles))
   (:gen-class))
 
 ;; Azulenoid-specific functions
 
 (def template
-  (DSymbol. (str "1.1:60:"
-                 "2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 "
-                 "32 34 36 38 40 42 44 46 48 50 52 54 56 58 60,"
-                 "6 3 5 12 9 11 18 15 17 24 21 23 30 27 29 36 "
-                 "33 35 42 39 41 48 45 47 54 51 53 60 57 59,"
-                 "0 0 12 11 28 27 0 0 18 17 36 35 24 23 58 57 30 "
-                 "29 0 0 0 0 42 41 0 0 48 47 0 0 54 53 0 0 60 59 0 0:"
-                 "3 3 3 3 3 3 3 3 3 3,0 0 5 0 7 0 0 0 0 0")))
+  (dsymbol (str "1.1:60:"
+                "2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 "
+                "32 34 36 38 40 42 44 46 48 50 52 54 56 58 60,"
+                "6 3 5 12 9 11 18 15 17 24 21 23 30 27 29 36 "
+                "33 35 42 39 41 48 45 47 54 51 53 60 57 59,"
+                "0 0 12 11 28 27 0 0 18 17 36 35 24 23 58 57 30 "
+                "29 0 0 0 0 42 41 0 0 48 47 0 0 54 53 0 0 60 59 0 0:"
+                "3 3 3 3 3 3 3 3 3 3,0 0 5 0 7 0 0 0 0 0")))
 
 (def octagon
-  (DSymbol. "1.1:16 1:2 4 6 8 10 12 14 16,16 3 5 7 9 11 13 15:8"))
+  (dsymbol "1.1:16 1:2 4 6 8 10 12 14 16,16 3 5 7 9 11 13 15:8"))
 
 (defn boundary-chambers [ds D i j k]
   (iterate-cycle [#(s ds i %1) #(chain-end ds %1 j k)] D))
 
 (def boundary-mappings
-  (let [template-boundary (boundary-chambers template (Integer. 1) 0 1 2)
-        octagon-boundary (cycle (map #(Integer. %1) (range 1 17)))]
+  (let [template-boundary (boundary-chambers template 1 0 1 2)
+        octagon-boundary (cycle (range 1 17))]
     (for [p (range 1 16 2)]
       (zipmap (drop (mod (- 19 p) 16) octagon-boundary)
               (take 16 template-boundary)))))
 
 (defn on-template [ds oct2tmp]
-  (let [result (DynamicDSymbol. template)]
-    (doseq [[D D*] oct2tmp :when (not (s result 2 D*))]
-      (.redefineOp result 2 D* (oct2tmp (s ds 2 D))))
-    (doseq [[D D*] oct2tmp :when (not (v result 1 2 D*))]
-      (.redefineV result 1 2 D* (v ds 1 2 D)))
-    result))
+  (let [glue-mapped (fn [tmp [D D*]] (glue tmp 2 D* (oct2tmp (s ds 2 D))))
+        spin-mapped (fn [tmp [D D*]] (spin tmp 1 2 D* (v ds 1 2 D)))
+        glued (reduce glue-mapped template oct2tmp)
+        spun (reduce spin-mapped glued oct2tmp)]
+    spun))
 
 (def octa-sets (filter #(-> % (curvature 1) (>= 0))
-                       (lazy-seq (CombineTiles. octagon))))
+                       (lazy-seq (CombineTiles. (java-dsymbol octagon)))))
 
 (def octa-syms
   (for [dset octa-sets
@@ -55,7 +53,8 @@
     dsym))
 
 (def azul-syms
-  (let [raw (for [ds octa-syms o2t boundary-mappings] (on-template ds o2t))]
+  (let [raw (for [ds octa-syms o2t boundary-mappings]
+              (java-dsymbol (on-template ds o2t)))]
     (for [ds (unique #(-> %1 .minimal .invariant) raw)]
       (-> ds .dual .minimal .canonical))))
 
