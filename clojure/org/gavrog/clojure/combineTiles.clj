dbadb9ac09ac226df2cfc1b61236a54276509ac7
odf <olaf.delgado@gmail.com>
2012-12-28 15:14:14 +1100

    combineTiles.clj: started writing the backtracker

--- clojure/org/gavrog/clojure/combineTiles.clj
+++ clojure/org/gavrog/clojure/combineTiles.clj
@@ -16,14 +16,30 @@
   (for [orb (partition-by-automorphism-group ds)]
     (canonical ds (first orb))))
 
-(defn- signatures [ds]
-  (into {} (for [orb (partition-by-automorphism-group ds)
-                 :let [inv (invariant ds (first orb))],
-                 D orb]
-             [D inv])))
+(defn- signatures [ds, idcs]
+  (into {} (for [D (orbit-reps ds idcs)
+                 :let [sub (orbit ds idcs D)]
+                 block (partition-by-automorphism-group sub)
+                 :let [inv (invariant sub (first block))],
+                 E block]
+             [E inv])))
 
 (defn combine-tiles [ds]
-  (make-backtracker 
-    {:root []
-     :extract (fn [[]])
-     :children (fn [[]])}))
+  (let [d (dim ds)
+        face-idcs (range d)
+        counts (components-with-multiplicities ds)
+        parts (keys counts)
+        forms (into {} (for [sub parts] [sub (inequivalent-forms sub)]))
+        sigs (into {} (for [sub parts] [sub (signatures sub face-idcs)]))]
+    (make-backtracker 
+      {:root (let [sub (first parts)]
+               [sub
+                (sigs sub)
+                (orbit-reps sub face-idcs)
+                (assoc counts sub (dec (counts sub)))])
+       :extract (fn [[symbol _ free-elements free-components]]
+                  (when (and (empty? free-elements)
+                             (every? (comp zero? second) free-components)
+                             (canonical? symbol))
+                    symbol))
+       :children (fn [[]])})))
