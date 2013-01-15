9c5273e84bbe01380b8bb80d78864746d6c239ea
odf <olaf.delgado@gmail.com>
2013-01-15 22:59:29 +1100

    delaney2d.clj: new function (orbifold-symbol)

--- clojure/org/gavrog/clojure/delaney2d.clj
+++ clojure/org/gavrog/clojure/delaney2d.clj
@@ -1,19 +1,22 @@
 (ns org.gavrog.clojure.delaney2d
   (:use (org.gavrog.clojure delaney)))
 
+(defn- index-pairs [ds]
+  (let [[i j k] (indices ds)]
+    [[i j] [i k] [j k]]))
+
 (defn curvature
   ([ds default-v]
     (do
       (assert (= 2 (dim ds)) (str "Expected 2d symbol, got " (dim ds) "d"))
       (assert (connected? ds) "Symbol must be connected")
-      (let [[i j k] (indices ds)]
-        (reduce +
-                (- (size ds))
-                (for [[i j] [[i j] [i k] [j k]]
-                      :let [s #(if (orbit-loopless? ds [i j] %) 2 1)
-                            v #(or (v ds i j %) default-v)]
-                      D (orbit-reps ds [i j])]
-                  (/ (s D) (v D)))))))
+      (reduce +
+              (- (size ds))
+              (for [[i j] (index-pairs ds)
+                    :let [s #(if (orbit-loopless? ds [i j] %) 2 1)
+                          v #(or (v ds i j %) default-v)]
+                    D (orbit-reps ds [i j])]
+                (/ (s D) (v D))))))
   ([ds]
     (curvature ds 0)))
 
@@ -26,8 +29,7 @@
 (defn spherical? [ds]
   (and (pos? (curvature ds 1))
        (let [ds (oriented-cover ds)
-             [i j k] (indices ds)
-             cones (for [[i j] [[i j] [i k] [j k]]
+             cones (for [[i j] (index-pairs ds)
                          D (orbit-reps ds [i j])
                          :when (< 1 (v ds i j D))]
                      (v ds i j D))
@@ -35,3 +37,24 @@
          (and (not= 1 n) (or (not= 2 n) (= (first cones) (second cones)))))))
 
 (defn proto-spherical? [ds] (pos? (curvature ds 1)))
+
+(defn orbifold-symbol [ds]
+  (let [curv (curvature ds)
+        [i j k] (indices ds)
+        types (for [[i j] (index-pairs ds)
+                    D (orbit-reps ds [i j])
+                    :when (< 1 (v ds i j D))]
+                [(v ds i j D) (orbit-loopless? ds [i j] D)])
+        cones (for [[v b] types, :when b] v)
+        corners (for [[v b] types, :when (not b)] v)
+        cost (- 2 (+ (/ curv 2)
+                     (reduce + (map #(/ (dec %) %) cones))
+                     (reduce + (map #(/ (dec %) (* 2 %)) corners))
+                     (if (loopless? ds) 0 1)))
+        tmp (concat (sort cones)
+                    (if (loopless? ds) [] ["*"])
+                    (sort corners)
+                    (if (weakly-oriented? ds)
+                      (repeat (/ cost 2) "o")
+                      (repeat cost "x")))]
+    (if (pos? (count tmp)) (apply str tmp) "1")))
