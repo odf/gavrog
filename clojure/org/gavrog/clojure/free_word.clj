69c33594e0a42ffd7653f81ccc762a32cd390475
odf <olaf.delgado@gmail.com>
2013-01-19 14:30:55 +1100

    free-word.clj: various improvements

--- clojure/org/gavrog/clojure/free_word.clj
+++ clojure/org/gavrog/clojure/free_word.clj
@@ -2,17 +2,17 @@
 
 (defn normalise [xs]
   (assert (every? integer? xs))
-  (reduce (fn [w x]
-            (cond (= 0 x)
-                  w
-                  (= (last w) (- x))
-                  (pop w)
-                  :else
-                  (conj w x)))
-          []
-          xs))
+  (reduce (fn [w x] (cond (= 0 x)
+                          w,
+                          (= (last w) (- x))
+                          (pop w),
+                          :else
+                          (conj w x)))
+          [] xs))
 
 (defn- overlap [w1 w2]
+  (assert (every? integer? w1))
+  (assert (every? integer? w2))
   (let [n1 (count w1)
         n2 (count w2)
         n (min n1 n2)
@@ -30,15 +30,28 @@
     (reduce product (product w1 w2) ws)))
 
 (defn inverse [w]
+  (assert (every? integer? w))
   (into [] (map - (reverse w))))
 
 (defn raised-to [w m]
-  (let [n (count w)
-        k (overlap w w)]
-    (if (= k n)
-      (if (odd? m) w [])
-      (let [head (subvec w 0 (- (dec n) k))
-            tail (subvec w (inc k))
-            mid (subvec w k (- n k))]
-        (into (reduce into head (repeat m mid)) tail)))))
+  (cond
+    (zero? m)
+    [],
+    (neg? m)
+    (raised-to (inverse w) (- m)),
+    :else
+    (let [n (count w)
+          k (overlap w w)]
+      (if (= k n)
+        (if (odd? m) w [])
+        (let [head (subvec w 0 (- (dec n) k))
+              tail (subvec w (inc k))
+              mid (subvec w k (- n k))]
+          (into (reduce into head (repeat m mid)) tail))))))
 
+(defn commutator [a b]
+  (product a b (inverse a) (inverse b)))
+
+(def w* product)
+
+(def w** raised-to)
