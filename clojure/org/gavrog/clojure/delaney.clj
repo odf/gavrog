63cd68b451357c2b4bd373093b25acb68115e5d1
odf <olaf.delgado@gmail.com>
2013-02-16 19:15:47 +1100

    delaney.clj: reimplemented (oriented-cover) via the generic (cover) function

--- clojure/org/gavrog/clojure/delaney.clj
+++ clojure/org/gavrog/clojure/delaney.clj
@@ -579,33 +579,6 @@
   ([ds1 ds2 & xds]
     (reduce append (append ds1 ds2) xds)))
 
-(defn- oc-pairs [ori n D E]
-  (cond (= D E)
-        [[D (+ D n)] [(+ D n) D]],
-        (= (ori D) (ori E))
-        [[D (+ E n)] [(+ E n) D] [(+ D n) E] [E (+ D n)]],
-        :else
-        [[D E] [E D] [(+ D n) (+ E n)] [(+ E n) (+ D n)]]))
-
-(defn oriented-cover [ds]
-  (if (oriented? ds)
-    ds
-    (let [ds (dsymbol ds)
-          ori (partial-orientation ds)
-          d (dim ds)
-          n (size ds)
-          ops (into {} (for [i (range (inc d))]
-                         [i (into {} (apply concat
-                                            (for [D (elements ds)
-                                                  :let [E (s ds i D)]]
-                                              (oc-pairs ori n D E))))]))
-          vs (into {} (for [i (range d)]
-                        [i (into {} (apply concat
-                                           (for [D (elements ds)
-                                                 :let [x (v ds i (inc i) D)]]
-                                             [[D x] [(+ D n) x]])))]))]
-      (DSymbol. d (+ n n) ops vs))))
-
 (defn cover [ds nr-cosets action]
   (let [ds (dsymbol ds)
         idcs (indices ds)
@@ -621,9 +594,19 @@
                       [i (into {} (for [D (orbit-reps t [i j])
                                         :let [D0 (-> D dec (mod n) inc)
                                               v* (/ (m ds i j D0) (r t i j D))]
-                                        E (orbit t [i j] D)]
+                                        E (orbit-elements t [i j] D)]
                                     [E v*]))]))]
     (DSymbol. d (size t) ops vs)))
+
+(defn oriented-cover [ds]
+  (if (oriented? ds)
+    ds
+    (let [ds (dsymbol ds)
+          ori (partial-orientation ds)]
+      (cover ds 2 (fn [k i D]
+                    (if (= (ori D) (ori (s ds i D)))
+                      (- 1 k)
+                      k))))))
 
 ;; === Tests
 
