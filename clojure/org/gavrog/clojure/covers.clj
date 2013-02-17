9008c66c0a7bc6908bcd447bd3ccd47fe0028aca
odf <olaf.delgado@gmail.com>
2013-02-17 19:02:37 +1100

    covers,clj: removed an arity; added a function

--- clojure/org/gavrog/clojure/covers.clj
+++ clojure/org/gavrog/clojure/covers.clj
@@ -4,12 +4,12 @@
          fundamental
          cosets)))
 
-(defn subgroup-cover
-  ([ds fund-group subgroup-gens]
-    (let [{:keys [nr-generators relators edge-to-word]} fund-group
-          table (coset-table nr-generators relators subgroup-gens)]
-      (cover ds (count table) (fn [k i D]
-                                (reduce (fn [k g] ((table k) g))
-                                        k (edge-to-word [D i]))))))
-  ([ds subgroup-gens]
-    (subgroup-cover ds (fundamental-group ds) subgroup-gens)))
+(defn subgroup-cover [ds subgroup-gens]
+  (let [{:keys [nr-generators relators edge-to-word]} (fundamental-group ds)
+        table (coset-table nr-generators relators subgroup-gens)]
+    (cover ds (count table) (fn [k i D]
+                              (reduce (fn [k g] ((table k) g))
+                                      k (edge-to-word [D i]))))))
+
+(defn finite-universal-cover [ds]
+  (subgroup-cover ds []))
