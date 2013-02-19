90b307f29468806c4a74bfb3c6a1c7ff03cdda3d
odf <olaf.delgado@gmail.com>
2013-02-19 15:33:48 +1100

    covers.clj: refactored and added a function

--- clojure/org/gavrog/clojure/covers.clj
+++ clojure/org/gavrog/clojure/covers.clj
@@ -2,14 +2,23 @@
   (:use (org.gavrog.clojure
          delaney
          fundamental
-         cosets)))
+         cosets
+         [generators :only [results]])))
+
+(defn- cover-for-table [ds table edge-to-word]
+  (cover ds (count table) (fn [k i D]
+                            (reduce (fn [k g] ((table k) g))
+                                    k (edge-to-word [D i])))))
 
 (defn subgroup-cover [ds subgroup-gens]
   (let [{:keys [nr-generators relators edge-to-word]} (fundamental-group ds)
         table (coset-table nr-generators relators subgroup-gens)]
-    (cover ds (count table) (fn [k i D]
-                              (reduce (fn [k g] ((table k) g))
-                                      k (edge-to-word [D i]))))))
+    (cover-for-table ds table edge-to-word)))
 
 (defn finite-universal-cover [ds]
   (subgroup-cover ds []))
+
+(defn covers [ds max-degree]
+  (let [{:keys [nr-generators relators edge-to-word]} (fundamental-group ds)]
+    (for [table (results (tables-generator nr-generators relators max-degree))]
+      (cover-for-table ds table edge-to-word))))
