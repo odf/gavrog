fd59f82cedfc1166f78f782a015b2d588288a01e
odf <olaf.delgado@gmail.com>
2013-02-26 14:57:43 +1100

    delaney3d.clj: more work on pseudo-toroidal cover function

--- clojure/org/gavrog/clojure/delaney3d.clj
+++ clojure/org/gavrog/clojure/delaney3d.clj
@@ -4,6 +4,7 @@
           delaney
           fundamental
           cosets
+          covers
           [generators :only [results]])))
 
 (defn massoc [m k v]
@@ -16,26 +17,33 @@
   (every? true? (for [row (vals ct), g (keys row)] (= (row g) (row (- g))))))
 
 (defn- trace-word [ct k wd]
-  (reduce (fn [[k g]] ((ct k) g)) k wd))
+  (reduce (fn [k g] ((ct k) g)) k wd))
+
+(defn- degree [ct wd]
+  (some (fn [[i k]] (when (zero? k) i))
+        (rest (reductions (fn [[_ k] i] [(inc i) (trace-word ct k wd)])
+                          [0 0] (range)))))
 
 (defn- flattens? [ct [wd deg]]
-  (= deg (some (fn [[i k]] (when (zero? k) i))
-               (reductions (fn [[i k]] [(inc i) (trace-word ct k wd)])
-                           [0 0]
-                           (range (inc deg))))))
+  (= deg (degree ct wd)))
 
 (defn- flattens-all? [ct cones]
   (every? (partial flattens? ct) cones))
 
 (defn pseudo-toroidal-cover [ds]
-  (let [{:keys [nr-gens relators cones edge-to-word]} (fundamental-group ds)
+  (let [ds (oriented-cover ds)
+        {:keys
+         [nr-generators relators cones edge-to-word]} (fundamental-group ds)
         cones2 (filter (fn [[wd deg]] (= deg 2)) cones)
         cones3 (filter (fn [[wd deg]] (= deg 3)) cones)
-        base (map core-table (results (table-generator nr-gens relators 4)))
-        good (for [ct base :when (flattens-all? ct cones)
-                   type (if (= (count ct) 4)
-                          (if (not (fully-involutive? ct)) :z4 :v4)
-                          (map core-type (count ct)))]
-               [type ct])
+        base (map core-table
+                  (results (table-generator nr-generators relators 4)))
+        good (for [ct base :when (flattens-all? ct cones)]
+               (let [type (if (= (count ct) 4)
+                            (if (fully-involutive? ct) :v4 :z4)
+                            (core-type (count ct)))]
+                 [type ct]))
         candidates (reduce (fn [m [k v]] (massoc m k v)) {} good)
-        ]))
+        
+        ]
+    candidates))
