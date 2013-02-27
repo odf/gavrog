4b5895bab53359eacbe1abafee4ffc45bb721bcb
odf <olaf.delgado@gmail.com>
2013-02-27 16:54:15 +1100

    arithmetic.clj: more work on matrix diagonalisation.

--- clojure/org/gavrog/clojure/arithmetic.clj
+++ clojure/org/gavrog/clojure/arithmetic.clj
@@ -29,3 +29,44 @@
       (stuff! (for [i rows] [[i j1] (+ (* (M [i j1]) f11) (* (M [i j2]) f12))]))
       (stuff! (for [i rows] [[i j2] (+ (* (M [i j1]) f21) (* (M [i j2]) f22))]))
       persistent!)))
+
+(defn- smallest [xs]
+  (when (seq xs)
+    (reduce (fn [a b] (if (neg? (compare a b)) a b)) (first xs) (rest xs))))
+
+(defn- clear-col [M rows cols]
+  (let [i0 (first rows)
+        j0 (first cols)]
+    (reduce (fn [M i]
+              (let [a (M [i0 j0]) b (M [i j0])]
+                (cond (and (not (zero? a)) (zero? (mod b a)))
+                      (combine-rows M cols i0 i 1 0 (- (quot b a)) 1)
+                      
+                      (not (zero? b))
+                      (let [[_ [f11 f12 f21 f22]] (gcdex a b)]
+                        (combine-rows M cols i0 i f11 f12 f21 f22))
+                      
+                      :else
+                      M)))
+            (rest rows))))
+
+(defn- clear-row [M rows cols])
+
+(defn- eliminate [M rows cols]
+  (let [M (clear-col M rows cols)
+        [M dirty] (clear-row M rows cols)]
+    (if dirty
+      (recur M rows cols)
+      M)))
+
+(defn diagonalized [M rows cols]
+  (if (or (empty? rows) (empty? cols))
+    M
+    (let [i0 (first rows)
+          j0 (first cols)
+          [_ ip jp] (smallest (for [i rows, j cols
+                                    :when (not (zero? (M [i j])))]
+                                [i j]))
+          M (combine-rows M cols i0 ip 0 1 1 0)
+          M (combine-cols M rows j0 jp 0 (sign (M [ip jp])) 1 0)]
+      (recur (eliminate M rows cols) (rest rows) (rest cols)))))
