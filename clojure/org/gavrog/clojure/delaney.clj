009fe8d405d5c1175b4638ed4f8c5f281cbf98aa
odf <olaf.delgado@gmail.com>
2013-05-04 14:32:57 +1000

    delaney.clj: fixed read-symbols

--- clojure/org/gavrog/clojure/delaney.clj
+++ clojure/org/gavrog/clojure/delaney.clj
@@ -636,7 +636,7 @@
 ;; === Reading a sequence of symbols from a file (very naive method for now)
 
 (defn read-symbols [filename]
-     (=> (-> filename slurp (split "<")) (drop 1) (map dsymbol)))
+     (->> (-> filename slurp (.split "<")) (drop 1) (map dsymbol)))
 
 ;; === Tests
 
