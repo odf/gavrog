551a9b8f4784c7f31f11c0f942efeab9eb4a81c6
odf <olaf.delgado@gmail.com>
2013-05-05 13:49:03 +1000

    orbifoldInvariant3d.clj: computes the raw orbifold graph

--- clojure/org/gavrog/clojure/orbifoldInvariant3d.clj
+++ clojure/org/gavrog/clojure/orbifoldInvariant3d.clj
@@ -1,3 +1,37 @@
 (ns org.gavrog.clojure.orbifoldInvariant3d
   (:use (org.gavrog.clojure
-          delaney)))
+          delaney
+          delaney2d)))
+
+(defn- orbifold-type [ds idcs D]
+  (case (count idcs)
+    0 "1"
+    1 (if (= D (s ds (first idcs) D)) "1*" "1")
+    2 (let [[i j] idcs, n (v ds i j D)]
+        (if (orbit-loopless? ds idcs D) (str n) (str n "*"))) 
+    3 (orbifold-symbol (orbit ds idcs D))))
+
+(defn- sublists
+  ([i j k] [[i j] [i k] [j k]])
+  ([i j]   [[i] [j]])
+  ([i]     []))
+
+(defn- raw-orbifold-graph [ds]
+  (let [[i j k m] (indices ds)
+        index-combinations [[i] [j] [k] [m]
+                            [i j] [i k] [i m] [j k] [j m] [k m]
+                            [i j k] [i j m] [i k m] [j k m]]
+        to-rep (into {} (for [idcs index-combinations
+                              D (orbit-reps ds idcs)
+                              E (orbit-elements ds idcs D)]
+                          [[idcs E] [idcs D]]))
+        sub-orbits (fn [idcs D]
+                     (let [o (orbit ds idcs D)]
+                       (for [is (apply sublists idcs)
+                             D (orbit-reps o is)]
+                         (to-rep [is D]))))]
+    (into {} (for [idcs index-combinations
+                   D (orbit-reps ds idcs)]
+               [[idcs D] {:type (orbifold-type ds idcs D)
+                          :elms (orbit-elements ds idcs D) ;; for debugging
+                          :subs (sub-orbits idcs D)}]))))
