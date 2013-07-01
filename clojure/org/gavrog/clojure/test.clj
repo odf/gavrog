a8f24c57b50ec3288729406015052e5b3f1924ef
odf <olaf.delgado@gmail.com>
2013-07-01 15:54:49 +1000

    Added some code to ladder test script

--- clojure/org/gavrog/clojure/test.clj
+++ clojure/org/gavrog/clojure/test.clj
@@ -2,9 +2,18 @@
 
 (def G (first (iterator-seq (org.gavrog.joss.pgraphs.io.Net/iterator "x.cgd"))))
 (def C (.getGraph (first (.connectedComponents G))))
-(def M (.minimalImage C))
+(def M (-> C .minimalImage .canonical))
 (def nodes (iterator-seq (.nodes M)))
 (def I (org.gavrog.joss.geometry.Operator/identity 3))
+
+(def phi
+  (org.gavrog.joss.pgraphs.basic.Morphism. (nth nodes 0) (nth nodes 1) I))
+
+
+(def L (first (iterator-seq (org.gavrog.joss.pgraphs.io.Net/iterator
+                              "ladder-1d.cgd"))))
+(def nodes (iterator-seq (.nodes L)))
+(def I (org.gavrog.joss.geometry.Operator/identity 1))
 
 (def phi
   (org.gavrog.joss.pgraphs.basic.Morphism. (nth nodes 0) (nth nodes 1) I))
