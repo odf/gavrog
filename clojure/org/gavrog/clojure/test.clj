93ff8fd47dab7db303fe75150fef5c0bcb3cc0db
odf <olaf.delgado@gmail.com>
2013-07-04 11:41:57 +1000

    Cleaned up Clojure test script fpor pgraph morphism problems

--- clojure/org/gavrog/clojure/test.clj
+++ clojure/org/gavrog/clojure/test.clj
@@ -8,12 +8,3 @@
 
 (def phi
   (org.gavrog.joss.pgraphs.basic.Morphism. (nth nodes 0) (nth nodes 1) I))
-
-
-(def L (first (iterator-seq (org.gavrog.joss.pgraphs.io.Net/iterator
-                              "ladder-1d.cgd"))))
-(def nodes (iterator-seq (.nodes L)))
-(def I (org.gavrog.joss.geometry.Operator/identity 1))
-
-(def phi
-  (org.gavrog.joss.pgraphs.basic.Morphism. (nth nodes 0) (nth nodes 1) I))
