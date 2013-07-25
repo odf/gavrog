c6cf4e7d818a25e79b0a0dd69203ac553d7fcd6d
odf <olaf.delgado@gmail.com>
2013-07-25 19:14:33 +1000

    Clojure: more work on new p-graph morphism detection ideas

--- clojure/org/gavrog/clojure/pgraph_morphisms.clj
+++ clojure/org/gavrog/clojure/pgraph_morphisms.clj
@@ -1,6 +1,7 @@
 (ns org.gavrog.clojure.pgraph-morphisms
-  (:import (org.gavrog.joss.pgraphs.io Net)
-           (org.gavrog.joss.geometry Operator)))
+  (:import (org.gavrog.joss.pgraphs.basic PeriodicGraph$CoverNode)
+           (org.gavrog.joss.pgraphs.io Net)
+           (org.gavrog.joss.geometry Operator Vector)))
 
 (defn nets [filename]
   (iterator-seq (Net/iterator filename)))
@@ -8,8 +9,18 @@
 (defn identity-matrix [net]
   (Operator/identity (.getDimension net)))
 
+(defn adjacent [node]
+  (map #(.target %) (iterator-seq (.incidences node))))
+
+(defn nextShellPair [[prev this]]
+  [this (set (for [u this, v (adjacent u) :when (not (prev v))] v))])
+
+(defn shells [net node]
+  (let [v0 (PeriodicGraph$CoverNode. net node)]
+    (iterate nextShellPair [#{v0} (set (adjacent v0))])))
+
 (defn node-signatures [net]
-  (let [mod-1 (fn [[v p]] [v (.modZ p)])
-        pos (into {} (map mod-1 (.barycentricPlacement net)))
-        by-pos (group-by pos (.nodes net))]
+  (let [pos (.barycentricPlacement net)
+        pos* (zipmap (keys pos) (map #(.modZ %) (vals pos)))
+        by-pos (group-by pos* (.nodes net))]
     by-pos))
