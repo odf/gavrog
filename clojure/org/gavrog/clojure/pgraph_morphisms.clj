132ac63c4a322c2fa0d25df388597e267ac89ec4
odf <olaf.delgado@gmail.com>
2013-07-26 10:26:46 +1000

    Clojure: further work on new p-graph morphism ideas

--- clojure/org/gavrog/clojure/pgraph_morphisms.clj
+++ clojure/org/gavrog/clojure/pgraph_morphisms.clj
@@ -6,21 +6,34 @@
 (defn nets [filename]
   (iterator-seq (Net/iterator filename)))
 
+(defn cover-node [net node]
+  (PeriodicGraph$CoverNode. net node))
+
+(defn cover-node-position [pos node]
+  (.plus (pos (.getOrbitNode node)) (.getShift node)))
+
 (defn identity-matrix [net]
   (Operator/identity (.getDimension net)))
 
 (defn adjacent [node]
   (map #(.target %) (iterator-seq (.incidences node))))
 
-(defn nextShellPair [[prev this]]
+(defn next-shell-pair [[prev this]]
   [this (set (for [u this, v (adjacent u) :when (not (prev v))] v))])
 
 (defn shells [net node]
-  (let [v0 (PeriodicGraph$CoverNode. net node)]
-    (iterate nextShellPair [#{v0} (set (adjacent v0))])))
+  (conj
+    (map second (iterate next-shell-pair [#{node} (set (adjacent node))]))
+    #{node}))
+
+(defn shell-positions [net node]
+  (let [pos (into {} (.barycentricPlacement net))]
+    (map (partial map (partial cover-node-position pos))
+         (shells net (cover-node net node)))))
 
 (defn node-signatures [net]
   (let [pos (.barycentricPlacement net)
         pos* (zipmap (keys pos) (map #(.modZ %) (vals pos)))
+        shifts (zipmap (keys pos) (map #(.minus (pos %) (pos* %)) (keys pos)))
         by-pos (group-by pos* (.nodes net))]
     by-pos))
