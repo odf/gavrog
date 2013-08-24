3ff5c69aa3e86690915c30c11bc2baed45adad2a
odf <olaf.delgado@gmail.com>
2013-08-24 20:20:08 +0200

    update CHANGELOG and Version.java

--- src/org/gavrog/apps/_3dt/Version.java
+++ src/org/gavrog/apps/_3dt/Version.java
@@ -23,9 +23,9 @@ public class Version {
 
 	final public static int patchLevel = 0;
 
-	final public static String maturity = "beta";
+	final public static String maturity = "beta2";
 
-	final public static String date = "2013/08/16";
+	final public static String date = "2013/08/24";
 
 	final public static String extension = (maturity == null ? "" : " "
             + maturity)
