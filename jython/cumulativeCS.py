262c8f5c5478f4ec2c9a704f2ff2771a1da83fc1
odf <olaf.delgado@gmail.com>
2020-05-17 15:55:25 +1000

    jython/cumulativeCS: new script

---
+++ jython/cumulativeCS.py
@@ -0,0 +1,24 @@
+#!/bin/env jython
+
+import sys
+import java.io
+
+from org.gavrog.joss.pgraphs.io import Net
+
+out = java.io.OutputStreamWriter(sys.stdout)
+
+
+for rawGraph in Net.iterator(sys.argv[1]):
+    out.write("\n## %s\n" % rawGraph.name)
+    out.flush()
+
+    G = rawGraph.minimalImage()
+    cs = G.coordinationSequence(G.nodes().next())
+
+    s = 0
+    for k in range(101):
+        x = cs.next()
+        s += x
+        if k > 0:
+            out.write("%4d  %8d  %10d  %8.4f\n" % (k, x, s, float(s) / k**3))
+        out.flush()
